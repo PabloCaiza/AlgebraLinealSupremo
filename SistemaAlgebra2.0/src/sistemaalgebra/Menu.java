857d22d63c2248ab923637ab06cf0cd7095469c9
PabloCaiza <48301519+PabloCaiza@users.noreply.github.com>
2019-05-14 21:35:53 -0500

    version 9.1

--- SistemaAlgebra2.0/src/sistemaalgebra/Menu.java
+++ SistemaAlgebra2.0/src/sistemaalgebra/Menu.java
@@ -35,6 +35,8 @@ public class Menu extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jButtonAdjunta = new javax.swing.JButton();
         jButtonDeterminante = new javax.swing.JButton();
+        jButtonDeterminante1 = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jEditorPane1 = new javax.swing.JEditorPane();
@@ -50,7 +52,7 @@ public class Menu extends javax.swing.JFrame {
                 jButtonInversaActionPerformed(evt);
             }
         });
-        getContentPane().add(jButtonInversa, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 280, 220, 50));
+        getContentPane().add(jButtonInversa, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 150, 220, 50));
 
         jButtonGauss.setFont(new java.awt.Font("Times New Roman", 0, 24)); // NOI18N
         jButtonGauss.setText("Gauss Jordan");
@@ -59,15 +61,15 @@ public class Menu extends javax.swing.JFrame {
                 jButtonGaussActionPerformed(evt);
             }
         });
-        getContentPane().add(jButtonGauss, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 190, 220, 50));
+        getContentPane().add(jButtonGauss, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 150, 220, 50));
 
         jLabel1.setFont(new java.awt.Font("Times New Roman", 0, 24)); // NOI18N
-        jLabel1.setText("Seleccione el método que desea usar");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 130, 530, -1));
+        jLabel1.setText("Seleccione el método que desea usar para resolver sistemas de ecuaciones: ");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 110, 740, -1));
 
         jLabel3.setFont(new java.awt.Font("Century Schoolbook", 1, 36)); // NOI18N
         jLabel3.setText("¡Bienvenido!");
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 40, 460, 60));
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 20, 460, 60));
 
         jButton1.setText("Salir");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -75,7 +77,7 @@ public class Menu extends javax.swing.JFrame {
                 jButton1ActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(720, 10, -1, -1));
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(810, 10, -1, -1));
 
         jButtonAdjunta.setFont(new java.awt.Font("Times New Roman", 0, 24)); // NOI18N
         jButtonAdjunta.setText("Adjunta");
@@ -84,7 +86,7 @@ public class Menu extends javax.swing.JFrame {
                 jButtonAdjuntaActionPerformed(evt);
             }
         });
-        getContentPane().add(jButtonAdjunta, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 280, 220, 50));
+        getContentPane().add(jButtonAdjunta, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 300, 220, 50));
 
         jButtonDeterminante.setFont(new java.awt.Font("Times New Roman", 0, 24)); // NOI18N
         jButtonDeterminante.setText("Determinante");
@@ -93,7 +95,20 @@ public class Menu extends javax.swing.JFrame {
                 jButtonDeterminanteActionPerformed(evt);
             }
         });
-        getContentPane().add(jButtonDeterminante, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 190, 220, 50));
+        getContentPane().add(jButtonDeterminante, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 300, 220, 50));
+
+        jButtonDeterminante1.setFont(new java.awt.Font("Times New Roman", 0, 24)); // NOI18N
+        jButtonDeterminante1.setText("Inversa");
+        jButtonDeterminante1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonDeterminante1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButtonDeterminante1, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 300, 220, 50));
+
+        jLabel4.setFont(new java.awt.Font("Times New Roman", 0, 24)); // NOI18N
+        jLabel4.setText("Seleccione lo que desea resolver para matrices nxn:");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 240, 740, -1));
 
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/fonfomate.jpg"))); // NOI18N
         getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 880, 430));
@@ -140,6 +155,14 @@ public class Menu extends javax.swing.JFrame {
         this.dispose();
     }//GEN-LAST:event_jButtonAdjuntaActionPerformed
 
+    private void jButtonDeterminante1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonDeterminante1ActionPerformed
+        // TODO add your handling code here:
+        VistaMatrizInversa a= new VistaMatrizInversa();
+        a.setVisible(true);
+        this.dispose();
+        
+    }//GEN-LAST:event_jButtonDeterminante1ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -179,12 +202,14 @@ public class Menu extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButtonAdjunta;
     private javax.swing.JButton jButtonDeterminante;
+    private javax.swing.JButton jButtonDeterminante1;
     private javax.swing.JButton jButtonGauss;
     private javax.swing.JButton jButtonInversa;
     private javax.swing.JEditorPane jEditorPane1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
 }
